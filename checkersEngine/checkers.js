ec5c474ef51e6b1f1f135f1cdb8c79deaf2e3857
Ryan Cryar <cryarr@flip3.engr.oregonstate.edu>
2018-06-06 15:11:59 -0700

    added comments of proposed workflow inside of the checkers engine

--- checkersEngine/checkers.js
+++ checkersEngine/checkers.js
@@ -1,11 +1,22 @@
 //Checkers engine
 function checkersEngine(boardArray, userNum, start_cell, end_cell){
-
-
+	/*compute the distance between the start_cell and end_cell. 
+	 *
+	 * 	if it is within 1 then continue with the move functionality.
+	 * 	check to make sure it is not a forward move 
+	 * 	if it is then clear the cells, return not a valid move to client side
+	 *
+	 * 	if it is within 1 and diagonal then return true 
+	 *
+	 * Call check if king
+	 *
+	 * we might want to think about how it is going to do the checking? Maybe do a two dimensional array representing the entire board.
+	 */
 
 }
 
 //setBoard sets up the board to the begining setup
+//	this is incredibly specific. we might want to change this. 
 function setBoard(boardArray){
   //set the black pieces (TOP)
   for(var i = 1; i < 23; i + 2){
@@ -17,3 +28,12 @@ function setBoard(boardArray){
     boardArray[i] = "RR";
   }
 }
+
+
+
+/* pass through the move. coordinates "); */
+
+
+
+
+
